b1bf96bc00a6818abc3f3fa98114d2be3a34a48b
JosephAcernese <JosephAcernese@gmail.com>
2022-07-19 15:17:24 -0400

    Removed next month, not nessecary. Double checked both cancellation functions, ensuring they work. Completed cleanMonth function

--- TraceDataCleaner.py
+++ TraceDataCleaner.py
@@ -28,21 +28,21 @@ def cleanCancelationsPre2012(dataframe):
     dataframe.drop(dataframe[dataframe["trc_st"] == "C"].index , inplace=True)
 
     #Obtain list of all possible days
-    list_of_days = dataframe.drop_duplicates(subset="trd_rpt_dt")["trd_rpt_dt"].to_numpy()
+    list_of_days = dataframe.drop_duplicates(subset="trd_exctn_dt")["trd_exctn_dt"].to_numpy()
 
     #Loop through every date in the data
     for day in list_of_days:
 
         #Create a temporary dataframe of all transactions on a given day and all cancelations on a given day
         #This is to help with the efficiency of the code, it will result in less comparisons overall
-        temp_dataframe = dataframe[dataframe["trd_rpt_dt"] == day]
-        temp_cancellations = cancellations[cancellations["trd_rpt_dt"] == day]
+        temp_dataframe = dataframe[dataframe["trd_exctn_dt"] == day]
+        temp_cancellations = cancellations[cancellations["trd_exctn_dt"] == day]
 
         #Iterate through every cancellation for a given day
-        for index2,row2 in temp_cancellations.iterrows():
+        for index,row in temp_cancellations.iterrows():
 
             #Remove any rows where the msg_seq_nb matches a given cancellation
-            dataframe.drop(temp_dataframe[temp_dataframe["msg_seq_nb"] == temp_cancellations["orig_msg_seq_nb"][index2]].index )
+            dataframe.drop(temp_dataframe[temp_dataframe["msg_seq_nb"] == row["orig_msg_seq_nb"]].index,inplace=True )
 
     #Returns the newly cleaned dataframe
     return dataframe
@@ -64,7 +64,7 @@ def cleanCancelationsPost2012(dataframe):
 
         
         #Create a temporary dataframe of all transactions on a given day and all cancelations on a given day
-        #This is to help with the efficiency of the code, it will result in less comparisons overall
+        #This is to help with the efficiency of the code, Dit will result in less comparisons overall
         temp_dataframe = dataframe[dataframe["trd_exctn_dt"] == day]
         temp_cancellations = cancellations[cancellations["trd_exctn_dt"] == day]
 
@@ -79,7 +79,7 @@ def cleanCancelationsPost2012(dataframe):
 
 
 #Function used to remove the reversals in the data before 2012
-def cleanReversalsPre2012(dataframe,next_month=False):
+def cleanReversalsPre2012(dataframe):
 
     #Seperate reversals from the rest of the transactions
     reversals = dataframe[dataframe["asof_cd"] == "R"].copy()
@@ -106,49 +106,47 @@ def cleanReversalsPre2012(dataframe,next_month=False):
 
 
 
-def cleanReversalsPost2012(dataframe,next_month=False):
+def cleanReversalsPost2012(dataframe):
 
     reversals = dataframe[dataframe["trc_st"] == "Y"].copy()
     dataframe.drop(dataframe[dataframe["trc_st"] == "Y"].index,inplace=True)
 
     #Obtain list of all possible days
-    list_of_days = dataframe.drop_duplicates(subset="trd_rpt_dt")["trd_rpt_dt"].to_numpy()
+    list_of_days = dataframe.drop_duplicates(subset="trd_exctn_dt")["trd_exctn_dt"].to_numpy()
 
-        
 
-    #Iterate through every cancellation for a given day
-    for index,row in reversals.iterrows():
+    #Iterate through every day
+    for day in list_of_days:
 
-        #Remove any rows where the msg_seq_nb matches a given cancellation
-        dataframe.drop(dataframe[ dataframe['cusip_id']==row['cusip_id'] &  dataframe['entrd_vol_qt']==row['entrd_vol_qt'] & dataframe['rptd_pr']==row['rptd_pr'] & dataframe['trd_exctn_dt']==row['trd_exctn_dt'] & dataframe['trd_exctn_tm']==row['trd_exctn_tm'] & dataframe['rpt_side_cd']==row['rpt_side_cd'] &  dataframe['cntra_mp_id']==row['cntra_mp_id'] & dataframe['msg_seq_nb']==row['orig_msg_seq_nb'] ].index )
+        #Create temp dataframes to help with efficiency
+        temp_dataframe = dataframe[dataframe["trd_exctn_dt"] == day]
+        temp_reversals = reversals[reversals["trd_exctn_dt"] == day]
+
+        #Iterate through every cancellation for a given day
+        for index,row in temp_reversals.iterrows():
+
+            #Remove any rows where the msg_seq_nb matches a given cancellation
+            dataframe.drop(temp_dataframe[ temp_dataframe['cusip_id']==row['cusip_id'] &  temp_dataframe['entrd_vol_qt']==row['entrd_vol_qt'] & temp_dataframe['rptd_pr']==row['rptd_pr'] & temp_dataframe['trd_exctn_tm']==row['trd_exctn_tm'] & temp_dataframe['rpt_side_cd']==row['rpt_side_cd'] &  temp_dataframe['cntra_mp_id']==row['cntra_mp_id'] & temp_dataframe['msg_seq_nb']==row['orig_msg_seq_nb'] ].index, inplace=True )
 
     return dataframe
 
 
-def cleanMonth(cur_month,next_month=False):
+def cleanMonth(cur_month):
 
     cur_month.dropna(subset=["cusip_id"],inplace=True)
 
-    if(next_month == False):
+    pre2012 = cur_month[trace_data["trd_rpt_dt"] < "2012-02-06"]
+    post2012 = cur_month[trace_data["trd_rpt_dt"] >= "2012-02-06"]
 
-        if(cur_month.head(1)["trd_rpt_dt"] < "2012-02-06"):
-            cleanCancelationsPre2012(cur_month)
-            cleanReversalsPre2012(cur_month)
+    if len(pre2012.index) > 0:
+        cleanCancelationsPre2012(pre2012)
+        cleanReversalsPre2012(pre2012)
+    
+    if len(post2012.index) > 0:
+        cleanCancelationsPost2012(post2012)
+        cleanReversalsPost2012(post2012)
 
-        else:
-            cleanCancelationsPost2012(cur_month)
-            cleanReversalsPost2012(cur_month)
 
-
-    else:
-
-        if(cur_month.head(1)["trd_rpt_dt"] < "2012-02-06"):
-            cleanCancelationsPre2012(cur_month)
-            cleanReversalsPre2012(cur_month,next_month = next_month)
-
-        else:
-            cleanCancelationsPost2012(cur_month)
-            cleanReversalsPost2012(cur_month, next_month)
 
 
 
@@ -220,17 +218,8 @@ length = len(fileName)
 
 for i in range(length):
 
-    if i == length - 1:
-
-        cur_month = pd.read_csv(fileNames[i], low_memory=False)
-        cleanMonth(cur_month)
-        
-
-    else:
-
-        cur_month = pd.read_csv(fileNames[i], low_memory=False)
-        next_month = pd.read_csv(fileNames[i+1],low_memory = False)
-        cleanMonth(cur_month,next_month=next_month)
+    cur_month = pd.read_csv(fileNames[i], low_memory=False)
+    cleanMonth(cur_month)
 
         
 
