455fcc6d60c6fb1c023671aecc9b906ba17262d6
JosephAcernese <JosephAcernese@gmail.com>
2022-08-15 12:14:36 -0400

    Fixed various bugs related to updating dataframes

--- TraceDataCleaner.py
+++ TraceDataCleaner.py
@@ -66,7 +66,7 @@ def cleanCancelationsPost2012():
         for index,row in temp_cancellations.iterrows():
 
             #Remove any rows where the msg_seq_nb matches a given cancellation
-            cur_month.drop(temp_dataframe[ temp_dataframe['cusip_id']==row['cusip_id'] &  temp_dataframe['entrd_vol_qt']==row['entrd_vol_qt'] & temp_dataframe['rptd_pr']==row['rptd_pr'] & temp_dataframe['trd_exctn_tm']==row['trd_exctn_tm'] & temp_dataframe['rpt_side_cd']==row['rpt_side_cd'] &  temp_dataframe['cntra_mp_id']==row['cntra_mp_id'] & temp_dataframe['msg_seq_nb']==row['msg_seq_nb'] ].index, inplace=True )
+            cur_month.drop(temp_dataframe[ (temp_dataframe['cusip_id']==row['cusip_id']) &  (temp_dataframe['entrd_vol_qt']==row['entrd_vol_qt']) & (temp_dataframe['rptd_pr']==row['rptd_pr']) & (temp_dataframe['trd_exctn_tm']==row['trd_exctn_tm']) & (temp_dataframe['rpt_side_cd']==row['rpt_side_cd']) &  (temp_dataframe['cntra_mp_id']==row['cntra_mp_id']) & (temp_dataframe['msg_seq_nb']==row['msg_seq_nb']) ].index, inplace=True )
 
 
 
@@ -74,14 +74,17 @@ def cleanCancelationsPost2012():
 def cleanReversalsPre2012():
 
     #Seperate reversals from the rest of the transactions
-    reversals = cur_month[ (cur_month["trd_rpt_dt"] >= "2012-02-06") & (cur_month["asof_cd"] == "R")]
-    cur_month.drop(reversals.index)
+    reversals = cur_month[ (cur_month["trd_rpt_dt"] < "2012-02-06") & (cur_month["asof_cd"] == "R")]
+    cur_month.drop(reversals.index,inplace=True)
 
     #Remove duplicates from reversals,note this step might not be needed
     reversals.drop_duplicates(subset=["trd_exctn_dt","cusip_id","trd_exctn_tm","rptd_pr","entrd_vol_qt","rpt_side_cd","cntra_mp_id","trd_rpt_dt","trd_rpt_tm","msg_seq_nb"],keep="first",inplace=True)
+    reversals.drop_duplicates(subset=["trd_exctn_dt","bond_sym_id","trd_exctn_tm","rptd_pr","entrd_vol_qt"],keep="first",inplace=True)
+
 
     #Obtain list of all possible days
     list_of_days = reversals.drop_duplicates(subset="trd_exctn_dt")["trd_exctn_dt"].to_numpy()
+
 
     for day in list_of_days:
 
@@ -93,9 +96,9 @@ def cleanReversalsPre2012():
 
             #Pull all transactions that match the current reversal
             matched_transactions = temp_dataframe[(temp_dataframe["trd_exctn_dt"] < row["trd_rpt_dt"]) & (temp_dataframe["cusip_id"] == row["cusip_id"]) & (temp_dataframe["trd_exctn_tm"] == row["trd_exctn_tm"]) & (temp_dataframe["rptd_pr"] == row["rptd_pr"]) & (temp_dataframe["entrd_vol_qt"] == row["entrd_vol_qt"]) & (temp_dataframe["rpt_side_cd"] == row["rpt_side_cd"]) & (temp_dataframe["cntra_mp_id"] == row["cntra_mp_id"])]
-            
+
             #If for some reason there's more than one that matches, keep only the first one
-            matched_transactions = matched_transactions.drop_duplicates(subset=["trd_exctn_dt","bond_sym_id","trd_exctn_tm","rptd_pr","entrd_vol_qt"],keep="first")
+            #matched_transactions = matched_transactions.drop_duplicates(subset=["trd_exctn_dt","bond_sym_id","trd_exctn_tm","rptd_pr","entrd_vol_qt"],keep="first")
 
             #Drop the index of the selected transaction
             cur_month.drop(matched_transactions.index,inplace=True)
@@ -122,7 +125,7 @@ def cleanReversalsPost2012():
         for index,row in temp_reversals.iterrows():
 
             #Remove any rows where the msg_seq_nb matches a given cancellation
-            cur_month.drop(temp_dataframe[ temp_dataframe['cusip_id']==row['cusip_id'] &  temp_dataframe['entrd_vol_qt']==row['entrd_vol_qt'] & temp_dataframe['rptd_pr']==row['rptd_pr'] & temp_dataframe['trd_exctn_tm']==row['trd_exctn_tm'] & temp_dataframe['rpt_side_cd']==row['rpt_side_cd'] &  temp_dataframe['cntra_mp_id']==row['cntra_mp_id'] & temp_dataframe['msg_seq_nb']==row['orig_msg_seq_nb'] ].index, inplace=True )
+            cur_month.drop(temp_dataframe[ (temp_dataframe['cusip_id']==row['cusip_id']) &  (temp_dataframe['entrd_vol_qt']==row['entrd_vol_qt']) & (temp_dataframe['rptd_pr']==row['rptd_pr']) & (temp_dataframe['trd_exctn_tm']==row['trd_exctn_tm']) & (temp_dataframe['rpt_side_cd']==row['rpt_side_cd']) & (temp_dataframe['cntra_mp_id']==row['cntra_mp_id']) & (temp_dataframe['msg_seq_nb']==row['orig_msg_seq_nb']) ].index, inplace=True )
 
 
 def cleanMonth():
@@ -164,7 +167,7 @@ for i in range(2002,2022):
 
 length = len(fileNames)
 
-for i in range(length):
+for i in range(length):3
 
     cur_month = pd.read_csv(fileNames[i], low_memory=False)
     cleanMonth()
@@ -175,34 +178,49 @@ for i in range(length):
 '''
 
 #This block of code is meant for my own personal debugging
-cur_month = pd.read_csv("./TestFile/TestData.csv", low_memory=False)
+cur_month = pd.read_csv("./TestFile/TestData3.csv", low_memory=False)
 
 
 print("FOR FIRST FILE BEFORE CLEAN")
 
 print("Total observations: " + str(len(cur_month.index)))
+print("Total NaN cusip_id: " + str(cur_month["cusip_id"].isnull().sum()))
 print("Total reversals: " + str(len(cur_month[cur_month["asof_cd"] == "R"].index)))
 print("Total cancellations: " + str(len(cur_month[(cur_month["trc_st"] == "C") | (cur_month["trc_st"] == "W")].index)))
 
 cleanMonth()
 
-cur_month.to_csv("./TestFile/Clean_TestData.csv",index=False)
+cur_month.to_csv("./TestFile/Clean_TestData3.csv",index=False)
 
 
 
 print("FOR FIRST FILE AFTER CLEAN")
 
 print("Total observations: " + str(len(cur_month.index)))
+print("Total NaN cusip_id: " + str(cur_month["cusip_id"].isnull().sum()))
 print("Total reversals: " + str(len(cur_month[cur_month["asof_cd"] == "R"].index)))
-print("Total cancellations: " + str(len(cur_month[(cur_month["trc_st"] == "C") | (cur_month["trc_st"] == "W")].index)))
+print("Total cancellations: " + str(len(cur_month[((cur_month["trc_st"] == "C") | (cur_month["trc_st"] == "W"))].index)))
 
 
-cur_month = pd.read_csv("./TestFile/TestData2.csv", low_memory=False)
-cur_month.to_csv("./TestFile/Clean_TestData2.csv",index=False)
+cur_month = pd.read_csv("./TestFile/TestData4.csv", low_memory=False)
+
+
+print("FOR SECOND FILE BEFORE CLEAN")
+
+print("Total observations: " + str(len(cur_month.index)))
+print("Total NaN cusip_id: " + str(cur_month["cusip_id"].isnull().sum()))
+print("Total reversals: " + str(len(cur_month[(cur_month["trc_st"] == "Y")].index)))
+print("Total cancellations: " + str(len(cur_month[((cur_month["trc_st"] == "X") | (cur_month["trc_st"] == "C"))].index)))
 
 cleanMonth()
 
+print("FOR SECOND FILE AFTER CLEAN")
 
+print("Total observations: " + str(len(cur_month.index)))
+print("Total NaN cusip_id: " + str(cur_month["cusip_id"].isnull().sum()))
+
+print("Total reversals: " + str(len(cur_month[(cur_month["trc_st"] == "Y")].index)))
+print("Total cancellations: " + str(len(cur_month[(cur_month["trc_st"] == "X") | (cur_month["trc_st"] == "C")].index)))
 
 
 
